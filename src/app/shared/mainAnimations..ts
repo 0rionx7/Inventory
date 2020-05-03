fc2cea844df241d1a4762c73d2005877be302279
Orionx7 Reloaded <karatzas.geo@gmail.com>
2020-05-03 11:42:34 +0300

    CssAnimated

--- src/app/shared/mainAnimations..ts
+++ src/app/shared/mainAnimations..ts
@@ -1,47 +1,9 @@
-import {
-  trigger,
-  state,
-  style,
-  transition,
-  query,
-  animateChild,
-  animate,
-  group,
-} from '@angular/animations';
+import { trigger, style, transition, animate } from '@angular/animations';
 
 export const sidenavAnimations = [
-  trigger('expandSidenav', [
-    state(
-      'true',
-      style({
-        width: '255px',
-      })
-    ),
-    transition('false <=> true', [
-      group([
-        query('@subMenus', animateChild(), { optional: true }),
-        animate('0.3s ease-in-out'),
-      ]),
-    ]),
-  ]),
   trigger('subMenus', [
     transition(':enter', [style({ height: 0 }), animate('0.3s ease-in')]),
     transition(':leave', [animate('0.3s ease-in', style({ height: 0 }))]),
-  ]),
-  trigger('subArrow', [
-    state(
-      'false',
-      style({
-        transform: 'translateX(0)',
-      })
-    ),
-    state(
-      'true',
-      style({
-        transform: 'rotate(90deg)',
-      })
-    ),
-    transition('true <=> false', [animate('0.3s')]),
   ]),
 ];
 
