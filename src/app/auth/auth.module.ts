2249758b590b6163613a07f43a4fc3b6bfcd2337
Orionx7 Reloaded <karatzas.geo@gmail.com>
2020-05-28 12:44:55 +0300

    1

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 
-import { StoreModule } from '@ngrx/store';
-
 import { CoreModule } from '../core/core.module';
 import { LoginComponent } from './components/login/login.component';
-import * as fromAuth from './store/reducers';
+import { StoreModule } from '@ngrx/store';
+import * as fromAuth from './store/reducers/auth.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { AuthEffects } from './store/effects/auth.effects';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CoreModule,
-    StoreModule.forFeature(fromAuth.authFeatureKey, fromAuth.reducers),
+    StoreModule.forFeature(fromAuth.authFeatureKey, fromAuth.reducer),
+    EffectsModule.forFeature([AuthEffects]),
   ],
   exports: [LoginComponent],
 })
--- src/app/auth/store/reducers/auth.reducer.ts
+++ src/app/auth/store/reducers/auth.reducer.ts
@@ -1,9 +1,16 @@
 import { Action, createReducer, on } from '@ngrx/store';
 
+import * as AuthActions from '../actions/auth.actions';
+
 export const authFeatureKey = 'auth';
 
 export interface State {}
 
 export const initialState: State = {};
 
-export const reducer = createReducer(initialState);
+export const reducer = createReducer(
+  initialState,
+
+  on(AuthActions.authSuccess, (state, action) => state),
+  on(AuthActions.authFailure, (state, action) => state)
+);
